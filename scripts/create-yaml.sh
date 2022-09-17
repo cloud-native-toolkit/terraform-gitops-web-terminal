20cd61a83d92f150310fd3bf22994dcfdd94a62e
Seillama <seillama@protonmail.com>
2022-09-17 10:32:27 +0200

    Updates helm chart
    Signed-off-by: Seillama <seillama@protonmail.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -2,12 +2,16 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
-CHART_DIR=$(cd "${MODULE_DIR}/chart"; pwd -P)
+CHART_DIR=$(cd "${MODULE_DIR}/charts/web-terminal"; pwd -P)
 
 NAME="$1"
 DEST_DIR="$2"
+VALUES_FILE="values.yaml"
 
 mkdir -p "${DEST_DIR}"
 cp -R "${CHART_DIR}"/* "${DEST_DIR}"
+if [[ -n "${VALUES_FILE}" ]] && [[ -n "${VALUES_CONTENT}" ]]; then
+  echo "${VALUES_CONTENT}" > "${DEST_DIR}/${VALUES_FILE}"
+fi
 
 find "${DEST_DIR}" -name "*"
